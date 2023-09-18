47098a7140eb4cff378a57a683e6734735f564e7
Nikhil Choudhary <116430126+Nikhil2072001@users.noreply.github.com>
2023-09-18 13:59:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
 //your code here!
 
+document.addEventListener("DOMContentLoaded", function () {
+    const list = document.getElementById("infi-list");
+    let itemCount = 10; // The initial number of list items
+
+    // Function to add more list items
+    function addMoreItems() {
+        const itemsToAdd = 2;
+        for (let i = 1; i <= itemsToAdd; i++) {
+            itemCount++;
+            const listItem = document.createElement("li");
+            listItem.textContent = "List Item " + itemCount;
+            list.appendChild(listItem);
+        }
+    }
+
+    // Check if the user has scrolled to the end of the list
+    list.addEventListener("scroll", function () {
+        const isAtEnd = list.scrollTop + list.clientHeight >= list.scrollHeight;
+
+        if (isAtEnd) {
+            addMoreItems();
+        }
+    });
+
+    // Initially add 10 list items
+    addMoreItems();
+});
